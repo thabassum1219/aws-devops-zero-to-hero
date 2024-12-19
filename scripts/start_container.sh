e1edab57390d67be9713217e2faceb72b8a59ad3
thabassum1219 <168541695+thabassum1219@users.noreply.github.com>
2024-12-19 15:20:38 +0530

    opt

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull thabassum1219/simple-python-flask-app
+echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 thabassum1219/simple-python-flask-app
+echo
