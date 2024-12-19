5df4beb399634b3111102e732f374f82464c0b2a
thabassum1219 <168541695+thabassum1219@users.noreply.github.com>
2024-12-19 15:27:01 +0530

    code changes

--- day-2/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull thabassum1219/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 thabassum1219/simple-python-flask-app
